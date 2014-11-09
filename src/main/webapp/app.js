2d45f0d8ce563d3d64bdedb98d52c35f0548e191
Furkan Zumrut <furkan@furkanzumrut.com>
2014-11-10 00:49:28 +0200

    Update

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,16 +1,7 @@
 function Hello($scope, $http) {
-    $http.get('http://localhost:8080/carsales/ws/rest/AutoService/getAutoById/16').
+    $http.get('http://54.72.196.42/carsales-1.0/ws/v/1/rest/AutoService/getAutoById/16').
         success(function(data) {
             $scope.greeting = data;
         });
 }
 
-var app = angular.module('myApp', ['ngGrid']);
-app.controller('MyCtrl', function($scope) {
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
-    $scope.gridOptions = { data: 'myData' };
-});
